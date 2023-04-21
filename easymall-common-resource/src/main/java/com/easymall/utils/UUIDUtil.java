ecc830f807ea4c1164176860fe99c1a103d6de37
西山对寂寥 <1781287641@qq.com>
2023-04-21 17:24:39 +0800

    添加项目公用的工具工程easymall-common-resoure和持久层工具工程easymall-common-repository

---
+++ easymall-common-resource/src/main/java/com/easymall/utils/UUIDUtil.java
@@ -0,0 +1,13 @@
+package com.easymall.utils;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+	public static String getUUID(){
+		UUID uuid=UUID.randomUUID();
+		return uuid.toString();
+	}
+	public static void main(String[] args) {
+		System.out.println(getUUID());
+	}
+}
