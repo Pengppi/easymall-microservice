430e62ba59ff96709fd9355987fd5c0a6d331531
西山对寂寥 <1781287641@qq.com>
2023-04-25 23:40:57 +0800

    新建Seckill秒杀系统

---
+++ easymall-microservice-seckill/src/main/java/cn/edu/scnu/seckill/StarterSeckillCenter.java
@@ -0,0 +1,18 @@
+/**
+ * @Author: Neo
+ * @Date: 2023/04/25 星期二 23:39:16 下午
+ * @Project: easymall-project
+ * @IDE: IntelliJ IDEA
+ **/
+
+package cn.edu.scnu.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterSeckillCenter {
+    public static void main(String[] args) {
+        SpringApplication.run(StarterSeckillCenter.class, args);
+    }
+}
