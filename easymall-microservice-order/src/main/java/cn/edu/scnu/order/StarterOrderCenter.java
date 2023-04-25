248e48353b44fa16414482f83ad96ec50479bfc0
西山对寂寥 <1781287641@qq.com>
2023-04-25 10:03:12 +0800

    add orderservice

---
+++ easymall-microservice-order/src/main/java/cn/edu/scnu/order/StarterOrderCenter.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: Neo
+ * @Date: 2023/04/25 星期二 9:50:55 上午
+ * @Project: easymall-project
+ * @IDE: IntelliJ IDEA
+ **/
+
+package cn.edu.scnu.order;
+
+
+public class StarterOrderCenter {
+}
